dcf27b2e9c2a4b8d980cf81579cb242d48b2a896
Chandankumar <chandanraayappab@gmail.com>
2022-06-23 19:56:51 +0530

    jenkins practice

--- src/test/java/com/SimpleJenkins/sample/Simple.java
+++ src/test/java/com/SimpleJenkins/sample/Simple.java
@@ -1,11 +1,44 @@
 package com.SimpleJenkins.sample;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Simple {
 	@Test
-public void simple()
-{
-	System.out.println("Sample Jenkins");
+	public void simple()
+	
+	{
+		System.out.println("Sample Jenkins");
+		WebDriver driver=null;
+	String browser="chrome";
+	String url="httpe://localhost:8888";
+	switch (browser) {
+	case "chrome":
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		break;
+	case"firefox":
+		WebDriverManager.firefoxdriver().setup();
+		driver=new FirefoxDriver();
+		break;
+	case "edge":
+		WebDriverManager.edgedriver().setup();
+		driver=new EdgeDriver();
+		break;
+	default:
+		System.out.println("please specify proper browser key");
+		WebDriverManager.firefoxdriver().setup();
+		driver=new FirefoxDriver();
+		break;
+	}
+	driver.get(url);
+	driver.close();
+	}
+	
 }
-}
+
